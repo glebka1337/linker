3cb4005b4c02da486ed970bea6ca42f5da7ab42f
gleb <gleb273p@gmail.com>
2026-01-09 08:02:10 +0200

    Removed 'God Object' resources, moved to isp

--- src/core/lifespan.py
+++ src/core/lifespan.py
@@ -1,7 +1,11 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from src.core.resources import app_resource_manager
 from src.core.logger import setup_logging
+from src.core.resources import Resources
+from src.db.qdrant import qdrant_client_manager
+from src.db.mongo import mongo_beanie_manager
+from src.db.rabbit import rabbitmq_conn_manager
+
 import logging
 
 
@@ -14,11 +18,20 @@ async def lifespan(app: FastAPI):
     
     logger.info(f"Creating all app resources.")
     try:
-        async with app_resource_manager() as resources:
+
+        async with (
+            mongo_beanie_manager() as mongo_client,
+            rabbitmq_conn_manager() as rabbitmq_conn
+        ):
+            resources = Resources(
+                mongo_client=mongo_client,
+                rabbitmq_conn=rabbitmq_conn
+            )
+            
             app.state.resources = resources
             yield
             logger.info(f"Closing all posible connection, clients...")
-            
+        
     except Exception as e:
         logger.critical(f"Error occured in a lifespan: {e}")
         raise e
--- src/core/resources.py
+++ src/core/resources.py
@@ -1,39 +1,10 @@
-# src/core/resorces.py
-from contextlib import asynccontextmanager
-from dataclasses import dataclass
+from pydantic import BaseModel
+from typing import Optional
 from qdrant_client import AsyncQdrantClient
 from motor.motor_asyncio import AsyncIOMotorClient
 from aio_pika.abc import AbstractRobustConnection
-from src.db.mongo import mongo_beanie_manager
-from src.db.qdrant import qdrant_client_manager
-from src.db.rabbit import rabbitmq_conn_manager
 
-import logging
-
-logger = logging.getLogger(__name__)
-
-@dataclass
-class Resources:
-    qdrant_client: AsyncQdrantClient
-    mongo_client: AsyncIOMotorClient
-    rabbitmq_conn: AbstractRobustConnection
-
-@asynccontextmanager
-async def app_resource_manager():
-
-    async with (
-        qdrant_client_manager() as qdrant,
-        mongo_beanie_manager() as mongo,
-        rabbitmq_conn_manager() as rabbitmq
-    ):
-        
-        logger.info("All resorces initialized!")
-        yield Resources(
-            qdrant_client=qdrant,
-            mongo_client=mongo,
-            rabbitmq_conn=rabbitmq
-        )
-    logger.info("Closing all possible connections...")
-    
-      
-    
+class Resources(BaseModel):
+    qdrant_client: Optional[AsyncQdrantClient] = None
+    mongo_client: Optional[AsyncIOMotorClient] = None
+    rabbitmq_conn: Optional[AbstractRobustConnection] = None
