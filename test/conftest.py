f11f7ebd789214dff565fc366df299a2fed26d12
gleb <gleb273p@gmail.com>
2026-01-12 08:39:41 +0200

    Old tests were removed

--- test/conftest.py
+++
@@ -1,7 +0,0 @@
-from test.http.client import http_client
-from test.db.mongo import (
-    mongo_client,
-    mongo_test_collection,
-    init_mongo_beanie
-)
-from test.docker.rabbit import rabbitmq_container
--- test/docker/client.py
+++
@@ -1,9 +0,0 @@
-"""Client will be used as fixture"""
-import pytest
-import docker
-
-@pytest.fixture(scope='session')
-def docker_client():
-    docker_cl = docker.from_env()
-    yield docker_cl
-    docker_cl.close()
